651b96c84ac8f218af159ce1126aaf8d39249bb4
Aaron Delasy <aaron@thelang.io>
2023-01-23 00:08:31 +0200

    Improve home page examples

--- controllers/home.mjs
+++ controllers/home.mjs
@@ -1,53 +1,53 @@
 import hljs from 'highlight.js'
 
-const registerCodeShell = 'curl https://ratelimit.org/register?requests_count=4&time_frame=1000'
+const registerCodeShell = 'curl \'https://ratelimit.org/register?requests_count=4&time_frame=1000\''
 
 const registerCodeJS = `import axios from 'axios'
 
-const params = 'requests_count=4&time_frame=1000'
-const res = await axios.get(\`https://ratelimit.org/register?\${params}\`)
+const params = { requests_count: 4, time_frame: 1000 }
+const res = await axios.get('https://ratelimit.org/register', { params })
 const data = res.data
 
 console.log(data)`
 
 const registerCodePython = `import requests
 
-params = 'requests_count=4&time_frame=1000'
-res = requests.get(f'https://ratelimit.org/register?{params}')
+params = [('requests_count', '4'), ('time_frame', '1000')]
+res = requests.get('https://ratelimit.org/register', params=params)
 data = res.text
 
 print(data)`
 
 const registerCodeThe = `import request from "the/request"
 
-params := "requests_count=4&time_frame=1000"
-res := request.get("https://ratelimit.org/register?" + params)
+params := { requests_count: "4", time_frame: "1000" }
+res := request.get("https://ratelimit.org/register", params: params)
 data := res.data.str()
 
 print(data)`
 
-const requestCodeShell = 'curl https://ratelimit.org/{id}?url=https%3A%2F%2Fexample.com'
+const requestCodeShell = 'curl \'https://ratelimit.org/{id}?url=https%3A%2F%2Fexample.com\''
 
 const requestCodeJS = `import axios from 'axios'
 
-const params = 'url=https%3A%2F%2Fexample.com'
-const res = await axios.get(\`https://ratelimit.org/{id}?\${params}\`)
+const params = { url: 'https://example.com' }
+const res = await axios.get('https://ratelimit.org/{id}', { params })
 const data = res.data
 
 console.log(data)`
 
 const requestCodePython = `import requests
 
-params = 'url=https%3A%2F%2Fexample.com'
-res = requests.get(f'https://ratelimit.org/{id}?{params}')
+params = [('url', 'https://example.com')]
+res = requests.get('https://ratelimit.org/{id}', params=params)
 data = res.text
 
 print(data)`
 
 const requestCodeThe = `import request from "the/request"
 
-params := "url=https%3A%2F%2Fexample.com"
-res := request.get("https://ratelimit.org/{id}?" + params)
+params := { url: "https://example.com" }
+res := request.get("https://ratelimit.org/{id}", params: params)
 data := res.data.str()
 
 print(data)`
